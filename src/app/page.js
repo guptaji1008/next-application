0fe4c69e5289996b53c6d9cd069f20977f83a1f1
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2023-09-23 14:59:50 +0530

    2nd lec (events, func, states)

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,27 @@
+"use client"
+import { useState } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+
+  const [name, setName] = useState("Aman")
+
+  const InnerComp = () => {
+    return (
+      <div>
+        <h2>Inner Component named {name}</h2>
+      </div>
+    )
+  }
+
   return (
     <main className={styles.main}>
-      <User name="Aman"/>
-      <User name="Anuj"/>
-      <User name="Compounder"/>
-      <h1>Home Page</h1>
+      <h1>Events | Functions | States</h1>
+      <h2>My name is {name}</h2>
+      <button onClick={() => setName(name==="Aman" ? "Umang" : "Aman")}>CLICK ME</button>
+      {/* we cant use events in next component directly, because in ssr, events don't work. firstly we have to make the component client side page, which is done using "use client" at the top of th codes. */}
+      <InnerComp/>
+      {/* {InnerComp()} */}
     </main>
-  )
-}
-
-const User = (props) => {
-  return (
-    <h2>My name is {props.name}</h2>
   )
 }
