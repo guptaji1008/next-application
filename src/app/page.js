cf7112dc18dc59e141c4dd2dbe60a05de9c8ec7d
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2023-09-24 23:26:08 +0530

    15th lec (font optimization)

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,26 @@
-import Image from "next/image";
-import picture from '../../public/moneyImage.jpg'
+import { Quicksand } from "next/font/google"
+import { Pacifico } from "next/font/google"
+
+const quicksand = Quicksand({
+  weight: "400",
+  subsets: ["vietnamese"],
+  display: "swap"
+})
+
+const pacifico = Pacifico({
+  weight: "400",
+  subsets: ["cyrillic"],
+  display: "swap"
+})
 
 export default function Home() {
 
   return (
     <main>
-      <h1>Image Optimization</h1>
-      <Image 
-      src={picture}
-      width={500}
-      height={400}
-      />
+      <h1>Font Optimization</h1>
+      <h1 className={quicksand.className}>Using Quicksand font from next fonts.</h1>
+      <h1 className={pacifico.className}>Using Pacifico font from next fonts.</h1>
+      
     </main>
   )
 }
